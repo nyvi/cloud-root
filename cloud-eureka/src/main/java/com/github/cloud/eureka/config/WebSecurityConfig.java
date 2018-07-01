772c9d6467fe8c4499caa1e7aff401a487bf3e2c
nyvi <kang.eternity@gmail.com>
2018-07-01 22:45:36 +0800

    feat(security):eureka security 配置

---
+++ cloud-eureka/src/main/java/com/github/cloud/eureka/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.github.cloud.eureka.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Security 配置
+ *
+ * @author : czk
+ * @date 2018-07-01
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
