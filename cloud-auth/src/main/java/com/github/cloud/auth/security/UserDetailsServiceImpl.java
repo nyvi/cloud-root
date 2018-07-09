8031e8c532317726d6b553ee5b68b0007f1a9634
nyvi <kang.eternity@gmail.com>
2018-07-09 18:57:53 +0800

    feat(auth):auth

---
+++ cloud-auth/src/main/java/com/github/cloud/auth/security/UserDetailsServiceImpl.java
@@ -0,0 +1,31 @@
+package com.github.cloud.auth.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 用户信息查询服务
+ *
+ * @author : czk
+ * @date 2018-07-09 16:25
+ */
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        final String pwd = "$2a$10$TJMeUjzV0QuRDkEhvMtw0u5QfpYNTqFKEfiRUOFz39gMkgJ6JC5Wu";
+        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+        grantedAuthorities.add(new SimpleGrantedAuthority("role_admin"));
+        return new User(username, pwd, grantedAuthorities);
+    }
+
+}
