4486030256d35d98981f9a748482a27c003a8ae4
nyvi <kang.eternity@gmail.com>
2018-07-28 08:24:11 +0800

    fix(gateway):网关

---
+++ cloud-gateway/src/main/java/com/github/cloud/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.github.cloud.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author : czk
+ * @date 2018-07-21
+ */
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
---
+++ cloud-gateway/src/main/java/com/github/cloud/gateway/config/ResourceServerConfiguration.java
@@ -0,0 +1,8 @@
+package com.github.cloud.gateway.config;
+
+/**
+ * @author : czk
+ * @date 2018-07-22
+ */
+public class ResourceServerConfiguration {
+}
