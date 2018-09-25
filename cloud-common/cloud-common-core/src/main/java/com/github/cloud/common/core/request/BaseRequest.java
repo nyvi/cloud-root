bd0f691993c4679b2a5f6775384606b1e2b21cde
nyvi <kang.eternity@gmail.com>
2018-09-25 18:52:53 +0800

    feat(upms):添加BaseRequest

---
+++ cloud-common/cloud-common-core/src/main/java/com/github/cloud/common/core/request/BaseRequest.java
@@ -0,0 +1,14 @@
+package com.github.cloud.common.core.request;
+
+import java.io.Serializable;
+
+/**
+ * 统一请求参数内容
+ *
+ * @author : czk
+ * @date 2018-09-25
+ */
+public class BaseRequest implements Serializable {
+
+    private static final long serialVersionUID = -8887383615871181134L;
+}
---
+++ cloud-modules/upms-service/upms-biz/src/main/java/com/github/cloud/upms/biz/request/UserRequest.java
@@ -0,0 +1,13 @@
+package com.github.cloud.upms.biz.request;
+
+import com.github.cloud.common.core.request.BaseRequest;
+
+
+/**
+ * @author : czk
+ * @date 2018-09-22
+ */
+public class UserRequest extends BaseRequest {
+
+    private static final long serialVersionUID = 188233919071414263L;
+}
